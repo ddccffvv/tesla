39a2a22b5f0cba228a86a50deace07c852a9c2cc
stmu <stijnmuylle@gmail.com>
2013-09-15 17:43:17 +0200

    showing pid in status.sh

--- admin-scripts/status.sh
+++ admin-scripts/status.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-data=`ps aux | grep -e "datadaemonscheduler.py" | grep -v grep`
-cred=`ps aux | grep -e "credentialmanager.py" | grep -v grep`
+data=`ps aux | grep -e "datadaemonscheduler.py" | grep -v grep | tr -s ' ' | cut -d" " -f 2`
+cred=`ps aux | grep -e "credentialmanager.py" | grep -v grep | tr -s ' ' | cut -d" " -f 2`
 
 if [ -z "$data" ]; then
 	echo -e "\e[1;31m[ERROR] Datadaemonscheduler not running.\e[0m"
 else
-	echo -e "\e[1;32m[OK] Datadaemonscheduler running.\e[0m"
+	echo -e "\e[1;32m[OK] Datadaemonscheduler running. ($data)\e[0m"
 fi
 if [ -z "$cred" ]; then
 	echo -e "\e[1;31m[ERROR] Credentialmanager not running.\e[0m"
 else
-	echo -e "\e[1;32m[OK] Credentialmanager running.\e[0m"
+	echo -e "\e[1;32m[OK] Credentialmanager running. ($cred)\e[0m"
 fi
